ec6ab69f682698e34f85f195aca92149466b182a
pf-test-20260312 <beau.conti+tines@cms.hhs.gov>
2026-03-13 16:03:11 -0400

    Update update_lists.py

--- scripts/update_lists.py
+++ scripts/update_lists.py
@@ -96,7 +96,7 @@ def get_tor_ips():
     return ips
 
 files_to_update = {
-  #'proton.txt': get_proton_ips(),
+  'proton.txt': get_proton_ips(),
   'tor.txt': get_tor_ips(),
   #'pia.txt': get_pia_ips(),
   #'nord.txt': get_nord_ips()
@@ -104,7 +104,7 @@ files_to_update = {
 
 for file_name, content in files_to_update.items():
     write_file(
-      'proton.txt',
+      file_name,
       content
     )
 
